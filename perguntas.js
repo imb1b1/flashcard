a9a67e5655273e23d1118679c9ea325c153efd84
imb1b1 <00001084340094sp@al.educacao.sp.gov.br>
2024-11-28 11:42:18 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O que é Latitude?',
+    'A latitude é a distância em graus medida entre qualquer ponto na superfície terrestre e a linha do equador, que corresponde ao paralelo de 0°.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matematica',
+    'Um carro consome 12 km/l e tem um tanque de 50 litros. Se o preço da gasolina é R$ 5,00 por litro, qual é o custo total para percorrer 300 km?',
+    'R$ 125,00.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Historia',
+    'Qual foi a principal causa da Revolução Industrial e como ela afetou a sociedade?',
+    'A principal causa da Revolução Industrial foi o avanço das tecnologias, como a máquina a vapor. Isso levou à industrialização, ao crescimento das cidades e à formação de novas classes sociais, como a classe operária e a burguesia. As condições de trabalho mudaram significativamente, afetando a vida das pessoas da época.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que são ribossomos e qual sua função nas células?',
+    'Ribossomos são estruturas que produzem proteínas nas células. Eles podem estar soltos no citoplasma ou ligados ao retículo endoplasmático. Sua função principal é "ler" o RNA mensageiro e montar as proteínas a partir de aminoácidos.'
 )
